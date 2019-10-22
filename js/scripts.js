e5a3ab25038464e26553a53ba7014bb18935630f
Student <example@example.com>
2019-10-22 11:42:35 -0700

    simplify js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,6 @@
 $(document).ready(function() {
-  $("button.dark").click(function() {
-    $("body").removeClass();
-    $("body").addClass("goDark");
-    $(".dark").toggle();
-    $(".light").toggle();
-  });
-  $("button.light").click(function() {
-    $("body").removeClass();
-    $("body").addClass("goLight");
-    $(".light").hide();
-    $(".dark").show();
+  $("button").click(function() {
+    $("body").toggleClass("goDark");
+    $("body").toggleClass("goLight");
   });
 });
